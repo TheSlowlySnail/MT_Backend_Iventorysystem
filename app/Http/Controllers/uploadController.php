6f1cfd5553d0d36999047c5a4298bfb388af6808
Helge Meiering <helge.meiering@gmail.com>
2018-05-02 21:58:54 +0200

    Whatever

--- app/Http/Controllers/uploadController.php
+++ app/Http/Controllers/uploadController.php
@@ -7,7 +7,11 @@ use Illuminate\Http\Request;
 class uploadController extends Controller
 {
     public function store(request $request){
-        return $request->file('image');
+        //
+        $image = $request->file('image');
+        $destinationPath = public_path('/images');
+        $image->move($destinationPath);
+        //return $request->file('image')->move(public_path('images'));
 
     }
 }
